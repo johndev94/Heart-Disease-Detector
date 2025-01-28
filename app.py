b39e5fc9de2f99a771be462075918b5d035a3eaa
John Devaney <johndevaney1@hotmail.com>
2025-01-28 18:36:58 +0000

    Added user entered numeric and categorical parameters to array

--- app.py
+++ app.py
@@ -62,8 +62,70 @@ ca = st.sidebar.slider("Number of Major Vessels Colored by Fluoroscopy", 0, 3, 0
 thal = st.sidebar.selectbox("Thal", ["Normal", "Fixed Defect", "Reversible Defect"])
 thal_dict = {"Normal": 1, "Fixed Defect": 2, "Reversible Defect": 3}
 
+if st.sidebar.button("Predict"):
+    # Load the model
+    model = joblib.load('heart_disease_model.pkl')
+    st.markdown("Model loaded successfully")
+
+    st.write("Model Parameters:")
+    st.write(model.get_params())
+
+    st.write("User Parameters:")
+    user_data = {
+        "age": age,
+        "gender": gender,
+        "cp": cp,
+        "trestbps": trestbps,
+        "chol": chol,
+        "fbs": fbs_numeric,
+        "restecg": restecg,
+        "thalach": thalach,
+        "exang": exang_numerica,
+        "oldpeak": oldpeak,
+        "slope": slope,
+        "ca": ca,
+        "thal": thal
+    }
+
+    st.write("User Numeric Parameters:")
+    user_predict_data = {
+        "age": age,
+        "gender": gender_numeric,
+        "cp": cp_dict[cp],
+        "trestbps": trestbps,
+        "chol": chol,
+        "fbs": fbs_numeric,
+        "restecg": restecg_dict[restecg],
+        "thalach": thalach,
+        "exang": exang_numerica,
+        "oldpeak": oldpeak,
+        "slope": slope_dict[slope],
+        "ca": ca,
+        "thal": thal_dict[thal]
+    }
+
+    st.write(user_data)
+    st.write(user_predict_data)
+    
 
 
+# st.markdown("""
+# ### Dataset Information:
+# - **age**: Age of the patient.
+# - **sex**: Gender (1 = male, 0 = female).
+# - **cp**: Chest pain type (categorical).
+# - **trestbps**: Resting blood pressure (in mm Hg).
+# - **chol**: Serum cholesterol in mg/dl.
+# - **fbs**: Fasting blood sugar > 120 mg/dl (1 = true, 0 = false).
+# - **restecg**: Resting electrocardiographic results (categorical).
+# - **thalach**: Maximum heart rate achieved.
+# - **exang**: Exercise-induced angina (1 = yes, 0 = no).
+# - **oldpeak**: ST depression induced by exercise relative to rest.
+# - **slope**: The slope of the peak exercise ST segment.
+# - **ca**: Number of major vessels (0-3) colored by fluoroscopy.
+# - **thal**: A categorical variable related to heart status.
+# - **target**: Target variable (1 = heart disease present, 0 = heart disease absent).
+# """)
 
 
 
