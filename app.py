fc37c345ec152ec37d97c987c708d48c6b82455a
John Devaney <johndevaney1@hotmail.com>
2025-01-28 18:07:28 +0000

    User can now input body information and is stored in variables

--- app.py
+++ app.py
@@ -28,7 +28,43 @@ st.title("Heart disease prediction")
 
 # Sidebar
 st.sidebar.header("Configuration")
+
 age = st.sidebar.slider("Age", 20, 100, 50)
+
+gender = st.sidebar.radio("Sex", ["Male", "Female"])
+gender_numeric = 1 if gender == "Male" else 0
+
+cp = st.sidebar.selectbox("Chest Pain Type", ["Typical Angina", "Atypical Angina", "Non-anginal Pain", "Asymptomatic"])
+cp_dict = {"Typical Angina": 0, "Atypical Angina": 1, "Non-anginal Pain": 2, "Asymptomatic": 3}
+
+trestbps = st.sidebar.slider("Resting Blood Pressure (mm Hg)", 90, 200, 120)
+
+chol = st.sidebar.slider("Serum Cholesterol (mg/dl)", 100, 600, 200)
+
+fbs = st.sidebar.radio("Fasting Blood Sugar > 120 mg/dl", ["True", "False"])
+fbs_numeric = 1 if fbs == "True" else 0
+
+restecg = st.sidebar.selectbox("Resting Electrocardiographic Results", ["Normal", "ST-T wave abnormality", "Left ventricular hypertrophy"])
+restecg_dict = {"Normal": 0, "ST-T wave abnormality": 1, "Left ventricular hypertrophy": 2}
+
+thalach = st.sidebar.slider("Maximum Heart Rate Achieved", 60, 220, 150)
+
+exang = st.sidebar.radio("Exercise-Induced Angina", ["Yes", "No"])
+exang_numerica = 1 if exang == "Yes" else 0
+
+oldpeak = st.sidebar.slider("ST Depression Induced by Exercise Relative to Rest", 0.0, 10.0, 2.0)   
+
+slope = st.sidebar.selectbox("Slope of the Peak Exercise ST Segment", ["Upsloping", "Flat", "Downsloping"]) 
+slope_dict = {"Upsloping": 0, "Flat": 1, "Downsloping": 2}
+
+ca = st.sidebar.slider("Number of Major Vessels Colored by Fluoroscopy", 0, 3, 0)
+
+thal = st.sidebar.selectbox("Thal", ["Normal", "Fixed Defect", "Reversible Defect"])
+thal_dict = {"Normal": 1, "Fixed Defect": 2, "Reversible Defect": 3}
+
+
+
+
 
 
 
